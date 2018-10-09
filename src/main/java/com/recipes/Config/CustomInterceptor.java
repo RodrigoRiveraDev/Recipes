b002007007a12e26c74230809762772ef0134c32
Rodrigo Rivera <rivera.rodrigo.dev@gmail.com>
2018-10-09 00:21:37 -0400

    [R-11] basic logger info added

--- src/main/java/com/recipes/Config/CustomInterceptor.java
+++ src/main/java/com/recipes/Config/CustomInterceptor.java
@@ -1,5 +1,7 @@
 package com.recipes.Config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,26 +10,32 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Component
-public class UserInterceptor implements HandlerInterceptor {
+public class CustomInterceptor implements HandlerInterceptor {
+
+    private static Logger log = LoggerFactory.getLogger(CustomInterceptor.class);
+
     @Override
     public void afterCompletion(HttpServletRequest request,
-                                HttpServletResponse response, Object object, Exception arg3) throws  Exception{
-        System.out.println("Request is complete!!!");
+                                HttpServletResponse response, Object object, Exception ex) throws  Exception{
+        String logInfo = "[afterCompletion][" + request. + "]";
+        if(ex != null) {
+            logInfo = logInfo + "[exception: " + ex.getMessage() + "]";
+        }
+        log.info(logInfo);
     }
 
     @Override
     public void postHandle(HttpServletRequest request,
                            HttpServletResponse response, Object object, ModelAndView model)
             throws Exception {
-        System.out.println(response.getStatus());
-        System.out.println(object.toString());
-        System.out.println("Handler execution is complete");
+        log.info("[postHandle][" + request + "]");
     }
 
     @Override
     public boolean preHandle(HttpServletRequest request,
                              HttpServletResponse response, Object object) throws Exception {
-        System.out.println("Before Handler execution");
+        log.info("[preHandle][" + request + "]" + "[" + request.getMethod()
+                + "]" + request.getRequestURI());
         return true;
     }
 }
--- src/main/java/com/recipes/Config/InterceptorConfig.java
+++ src/main/java/com/recipes/Config/InterceptorConfig.java
@@ -9,6 +9,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class InterceptorConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new UserInterceptor());
+        registry.addInterceptor(new CustomInterceptor());
     }
 }
