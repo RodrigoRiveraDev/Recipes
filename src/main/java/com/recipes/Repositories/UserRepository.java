745e37e7c9d5912a943781062ef2e82c8c84ffd9
Rodrigo Rivera <rivera.rodrigo.dev@gmail.com>
2018-10-10 22:30:32 -0400

    [R-13] annotations added

--- src/main/java/com/recipes/Repositories/UserRepository.java
+++ src/main/java/com/recipes/Repositories/UserRepository.java
@@ -21,6 +21,11 @@ public interface UserRepository extends CrudRepository<User, Long> {
     @Query("SELECT u FROM User u")
     List<User> allUsers();
 
+    /**
+     * @param fullName The user fullName to search for
+     * @param email The user email to search for
+     * @return It will return the entity with the provided fullName and email or null
+     */
     @Query("SELECT u FROM User u WHERE u.fullName = :fullName AND u.email = :email")
     User findUser(@Param("fullName") String fullName, @Param("email") String email);
 }
